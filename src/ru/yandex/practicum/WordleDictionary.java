0d3cea19009978301b26f765c60d230fba8284dd
Nazar Shmigelskiy <nazarshmigelskiy@yandex.ru>
2026-04-23 00:42:59 +0300

    Не разобрался как протестировать исключение при пустом словаре, пытался создать новый пустой файл и загружать через него, но чет не справился

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,6 +1,10 @@
 package ru.yandex.practicum;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.List;
+import java.util.Random;
 
 /*
 этот класс содержит в себе список слов List<String>
@@ -10,5 +14,50 @@ import java.util.List;
 public class WordleDictionary {
 
     private List<String> words;
+    private final Writer log;
 
+    public WordleDictionary(List<String> words, Writer log) {
+        this.words = words;
+        this.log = log;
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+
+    public String getRandomWord() {
+        Random random = new Random();
+        return words.get(random.nextInt(words.size()));
+    }
+
+    public String getLetterEquality(String userWord, String answer)
+            throws StringIndexOutOfBoundsException, IOException, WordNotFoundInDictionaryException {
+        if (userWord.equals(answer)) {
+            return "+++++";
+        } else if (!words.contains(userWord)) {
+            throw new WordNotFoundInDictionaryException("Этого слова нет в нашем словаре");
+        } else  {
+            StringBuilder sb = new StringBuilder(5);
+            for (int i = 0;  i < userWord.length(); i++) {
+                    if (answer.charAt(i) == userWord.charAt(i)) {
+                        sb.append('+');
+                    } else if (answer.contains(userWord.charAt(i) + "")) {
+                        sb.append('^');
+                    } else {
+                        sb.append('-');
+                }
+            }
+            log.write(String.format("Пользователем введено слово: %s%n", userWord));
+            log.write(String.format("Вывод в консоль: %s%n", sb));
+
+        return sb.toString();
+        }
+    }
+
+
+    public void printDictionary() {
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
 }
--- src/ru/yandex/practicum/WordleGame.java
+++ src/ru/yandex/practicum/WordleGame.java
@@ -1,5 +1,12 @@
 package ru.yandex.practicum;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 /*
 в этом классе хранится словарь и состояние игры
     текущий шаг
@@ -13,11 +20,82 @@ package ru.yandex.practicum;
 не забудьте про специальные типы исключений для игровых и неигровых ошибок
  */
 public class WordleGame {
-
     private String answer;
-
     private int steps;
-
     private WordleDictionary dictionary;
+    private final Writer log;
+    private boolean isWinner = false;
+    private HelpMeStepBro advisor = new HelpMeStepBro();
+
+    public WordleGame(Writer log, WordleDictionary dictionary, int steps, String answer) {
+        this.log = log;
+        this.dictionary = dictionary;
+        this.steps = steps;
+        this.answer = answer;
+    }
+
+
+    public String compareWords(String userInput) throws IOException, NotRightWordLengthException,
+            NotRussianLettersException, WordNotFoundInDictionaryException {
+        if (userInput.length() != 5) {
+            throw new NotRightWordLengthException("Слово должно состоять из 5 букв");
+        }
+        if (hasInvalidChars(userInput)) {
+            throw new NotRussianLettersException("Слово должно состоять только из русских букв");
+        }
+        String pattern = dictionary.getLetterEquality(formateWord(userInput), answer);
+        steps--;
+        advisor.update(userInput, pattern);
+        return pattern;
+    }
+
+
+    public boolean hasInvalidChars(String userInput) {
+        for (char c : userInput.toLowerCase().toCharArray()) {
+            if (!(c >= 'а' && c <= 'я') && c != 'ё') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String formateWord(String userInput) {
+        String formattedWord = userInput.toLowerCase();
+        if (formattedWord.contains("ё")) {
+            StringBuilder sb = new StringBuilder(formattedWord);
+            for (int i = 0; i < sb.length(); i++) {
+                if (sb.charAt(i) == 'ё') {
+                    sb.setCharAt(i, 'е');
+                }
+            }
+            formattedWord = sb.toString();
+        }
+        return formattedWord;
+    }
+
+    public String getAdvise() throws IOException {
+        log.write("Вызвана подсказка\n");
+        return advisor.imStuck(dictionary.getWords());
+
+    }
+
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public boolean isWinner() {
+        return isWinner;
+    }
+
+    public void setWinner(boolean winner) {
+        isWinner = winner;
+    }
 
 }
+
+
